35185fae32de4d49b8c34a8dcdc0215c232d57bc
kahosan <15116355657l@gmail.com>
2022-08-05 15:56:44 +0800

    chore: del useCallback

--- src/hooks/use-toasts.ts
+++ src/hooks/use-toasts.ts
@@ -1,11 +1,10 @@
 import { useToasts as useGeistToasts } from '@geist-ui/core';
-import { useCallback } from 'react';
 
 export const useToasts = () => {
   const { setToast: origSetToast, removeAll: origClearToasts } = useGeistToasts();
 
   return {
-    setToast: useCallback(origSetToast, []),
-    clearToasts: useCallback(origClearToasts, []),
+    setToast: origSetToast,
+    clearToasts: origClearToasts,
   };
 };
