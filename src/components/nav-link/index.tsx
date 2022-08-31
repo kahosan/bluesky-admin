4fac79e0223d2f5eeef2813d4e8a91f8f65ac18f
kahosan <15116355657l@gmail.com>
2022-08-31 13:31:12 +0800

    fix: link color of hover

--- src/components/nav-link/index.tsx
+++ src/components/nav-link/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
 import { NavLink as RouteNavLink } from 'react-router-dom';
 
-export const NavLink = ({ to, children }: { to: string; children?: React.ReactNode }) => <RouteNavLink to={to} style={{ color: 'inherit' }}>{children}</RouteNavLink>;
+import { Link } from '@geist-ui/core';
+
+export const NavLink = ({ to, children }: { to: string; children?: React.ReactNode }) => {
+  return <Link><RouteNavLink style={{ color: 'inherit' }} to={to}>{children}</RouteNavLink></Link>;
+};
