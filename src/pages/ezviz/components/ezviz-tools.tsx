a1162ee87ee7de154b8398f6e094697ee6374fe5
kahosan <15116355657l@gmail.com>
2022-10-13 17:02:29 +0800

    feat: new api

--- src/pages/ezviz/components/ezviz-tools.tsx
+++ src/pages/ezviz/components/ezviz-tools.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 import type { HTTPError } from '@/lib/fetcher';
 
-import type { EzvizCameraResp, EzvizCameraSearchResp, EzvizDeviceListResp } from '@/types/ezviz';
+import type { EzvizCameraSearchResp, EzvizDeviceListResp } from '@/types/ezviz';
 import { useTrigger } from '@/hooks/use-trigger';
 import { useToasts } from '@/hooks/use-toasts';
 
@@ -15,12 +15,12 @@ interface EzvizToolsProps {
 
 export const EzvizTools = ({ update }: EzvizToolsProps) => {
   const { setToast } = useToasts();
-  const [search, setSearch] = useState('');
+  const [keyword, setKeyword] = useState('');
 
-  const { trigger } = useTrigger<EzvizCameraSearchResp, HTTPError>('/api/camera/ezviz/info?');
+  const { trigger } = useTrigger<EzvizCameraSearchResp, HTTPError>('/api/camera/ezviz/search?');
 
   const handleSearch = async () => {
-    const data = await trigger({ deviceSerial: search });
+    const data = await trigger({ key: keyword });
 
     if (data) {
       if (data.code !== '200') {
@@ -31,12 +31,8 @@ export const EzvizTools = ({ update }: EzvizToolsProps) => {
         });
       }
 
-      // TODO 后端还没实现一些字段
-      const d = data.data as unknown as EzvizCameraResp;
-      d.addTime = d.updateTime;
       update({
         ...data,
-        data: [d],
         page: {
           total: 1,
           size: 1,
@@ -58,7 +54,7 @@ export const EzvizTools = ({ update }: EzvizToolsProps) => {
             onClick={() => handleSearch()}
           />
         }
-        onChange={e => setSearch(e.target.value)}
+        onChange={e => setKeyword(e.target.value)}
         className="mr-3"
       />
       <Button h={0.88}>添加设备</Button>
