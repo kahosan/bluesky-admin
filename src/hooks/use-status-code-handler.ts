78a921783f432209ae03ecfc5c2ea2258bc7f7a6
kahosan <15116355657l@gmail.com>
2022-10-31 14:03:25 +0800

    fix: message type

--- src/hooks/use-status-code-handler.ts
+++ src/hooks/use-status-code-handler.ts
@@ -6,20 +6,12 @@ export const useStatusCodeHandler = <T extends HTTPError>() => {
 
   return {
     handleError: (error: T) => {
-      if (error.status === 401) {
-        const err = error.info as { message: string };
-        setToast({
-          text: err.message,
-          type: 'error',
-          delay: 5000
-        });
-      } else {
-        setToast({
-          text: `${error.message}: ${error.status} ${error.info as string}`,
-          type: 'error',
-          delay: 5000
-        });
-      }
+      const err = error.info as { message: string };
+      setToast({
+        text: `${err.message}`,
+        type: 'error',
+        delay: 5000
+      });
     },
     handleSuccess: (data: { code: string; msg: string }) => {
       if (data.code !== '200') {
